cfaa954fd1ea2b99721b5f1cdbd3e65d923366aa
Steve <steve.maina@student.moringaschool.com>
2022-10-06 04:12:03 +0300

    function timer

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+function Footer(){
+    return(
+        <>
+       <ul className="Footer">
+        <li>Blake Photography Services</li>
+        <li>Location:Parliament Road Nairobi CBD</li>
+        <li>Contact Us:0112309874</li>
+       
+       </ul>
+
+
+        </>
+    )
+}
+    
+
+export default Footer;
